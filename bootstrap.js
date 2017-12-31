5294e4117206827dc801814b9e37a45aed777984
yehiyam <yehiyam@gmail.com>
2017-12-31 14:09:45 +0200

    supprt config

---
+++ bootstrap.js
@@ -0,0 +1,67 @@
+
+process.env.NODE_PATH = __dirname;
+require('module').Module._initPaths();
+
+const configIt = require('@hkube/config');
+const Logger = require('@hkube/logger');
+const VerbosityPlugin = require('@hkube/logger').VerbosityPlugin;
+let log;
+const serverInit = require('./lib/server');
+const etcdApi=require('./lib/etcd-data');
+
+class Bootstrap {
+    async init() {
+        try {
+            const { main, logger } = await configIt.load();
+            this._handleErrors();
+
+            log = new Logger(main.serviceName, logger);
+            log.plugins.use(new VerbosityPlugin(main.redis));
+            log.info('running application in ' + configIt.env() + ' environment', { component: 'main' });
+
+            await etcdApi.init(main);
+            await serverInit(main);
+            return main;
+        }
+        catch (error) {
+            log.error(error);
+            this._onInitFailed(new Error(`unable to start application. ${error.message}`));
+        }
+    }
+
+    _onInitFailed(error) {
+        if (log) {
+            log.error(error.message, { component: 'main' }, error);
+            log.error(error);
+        }
+        else {
+            console.error(error.message);
+            console.error(error);
+        }
+        process.exit(1);
+    }
+
+    _handleErrors() {
+        process.on('exit', (code) => {
+            log.info('exit' + (code ? ' code ' + code : ''), { component: 'main' });
+        });
+        process.on('SIGINT', () => {
+            log.info('SIGINT', { component: 'main' });
+            process.exit(1);
+        });
+        process.on('SIGTERM', () => {
+            log.info('SIGTERM', { component: 'main' });
+            process.exit(1);
+        });
+        process.on('unhandledRejection', (error) => {
+            log.error('unhandledRejection: ' + (error.message||error), { component: 'main' }, error);
+        });
+        process.on('uncaughtException', (error) => {
+            log.error('uncaughtException: ' + error.message, { component: 'main' }, error);
+            process.exit(1);
+        });
+    }
+}
+
+module.exports = new Bootstrap();
+
---
+++ config/main/config.base.js
@@ -0,0 +1,20 @@
+
+var package = require(process.cwd() + '/package.json');
+var config = module.exports = {};
+
+config.serviceName = package.name;
+
+config.rest = {
+    port: process.env.SIMULATOR_SERVER_REST_PORT || 3002,
+};
+const useCluster = process.env.REDIS_CLUSTER_SERVICE_HOST ? true : false;
+config.redis = {
+    host: useCluster ? process.env.REDIS_CLUSTER_SERVICE_HOST : process.env.REDIS_SERVICE_HOST || 'localhost',
+    port: useCluster ? process.env.REDIS_CLUSTER_SERVICE_PORT : process.env.REDIS_SERVICE_PORT || 6379,
+    cluster: useCluster
+};
+config.etcd = {
+    protocol: 'http',
+    host: process.env.ETCD_CLIENT_SERVICE_HOST || 'localhost2',
+    port: process.env.ETCD_CLIENT_SERVICE_PORT || 4001
+};
