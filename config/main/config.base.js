a11164ee2e258d70635222005c39193a6bee21da
Nassi Harel <nassi9@gmail.com>
2019-03-05 17:10:04 +0200

    add node input/output

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -1,18 +1,22 @@
 
-var package = require(process.cwd() + '/package.json');
-var config = module.exports = {};
+const package = require(process.cwd() + '/package.json');
+const config = module.exports = {};
+const useSentinel = !!process.env.REDIS_SENTINEL_SERVICE_HOST;
 
+config.defaultStorage = process.env.DEFAULT_STORAGE || 's3';
 config.serviceName = package.name;
 
 config.jaeger = {
     protocol: 'http',
     host: process.env.JAEGER_JAEGER_QUERY_SERVICE_HOST || '40.69.222.75',
     port: process.env.JAEGER_JAEGER_QUERY_SERVICE_PORT || 80
-}
+};
+
 config.kubernetes = {
     isLocal: !!process.env.KUBERNETES_SERVICE_HOST,
     namespace: process.env.NAMESPACE || 'default'
-}
+};
+
 config.apiServer = {
     protocol: 'http',
     host: process.env.API_SERVER_SERVICE_HOST || 'localhost',
@@ -23,21 +27,49 @@ config.apiServer = {
 config.debugUrl = {
     prefix: `hkube/debug`,
     suffix: `socket.io`
-
 }
 
 config.rest = {
     port: process.env.SIMULATOR_SERVER_REST_PORT || 30010,
 };
-const useSentinel = !!process.env.REDIS_SENTINEL_SERVICE_HOST;
 
 config.redis = {
     host: useSentinel ? process.env.REDIS_SENTINEL_SERVICE_HOST : process.env.REDIS_SERVICE_HOST || 'localhost',
     port: useSentinel ? process.env.REDIS_SENTINEL_SERVICE_PORT : process.env.REDIS_SERVICE_PORT || 6379,
     sentinel: useSentinel,
 };
+
 config.etcd = {
     protocol: 'http',
     host: process.env.ETCD_CLIENT_SERVICE_HOST || '127.0.0.1',
     port: process.env.ETCD_CLIENT_SERVICE_PORT || 4001
+};
+
+config.s3 = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'AKIAIOSFODNN7EXAMPLE',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+    endpoint: process.env.S3_ENDPOINT_URL || 'http://127.0.0.1:9000'
+};
+
+config.fs = {
+    baseDirectory: process.env.BASE_FS_ADAPTER_DIRECTORY || '/var/tmp/fs/storage'
+};
+
+config.storageAdapters = {
+    s3: {
+        connection: config.s3,
+        moduleName: process.env.STORAGE_MODULE || '@hkube/s3-adapter'
+    },
+    etcd: {
+        connection: config.etcd,
+        moduleName: process.env.STORAGE_MODULE || '@hkube/etcd-adapter'
+    },
+    redis: {
+        connection: config.redis,
+        moduleName: process.env.STORAGE_MODULE || '@hkube/redis-storage-adapter'
+    },
+    fs: {
+        connection: config.fs,
+        moduleName: process.env.STORAGE_MODULE || '@hkube/fs-adapter'
+    }
 };
