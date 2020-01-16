167bea62d698135de254eca2a9eac50f28ed9be9
Yehiyam Livneh <yehiyam@gmail.com>
2020-01-16 17:49:33 +0200

    filter missing executions

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -95,5 +95,5 @@ config.healthchecks = {
     path: process.env.HEALTHCHECK_PATH || '/healthz',
     port: process.env.HEALTHCHECK_PORT || '5000',
     maxDiff: process.env.HEALTHCHECK_MAX_DIFF || '30000',
-    logExternalRequests: formatter.parseBool(process.env.LOG_EXTERNAL_REQUESTS, true)
+    logExternalRequests: formatter.parseBool(process.env.LOG_EXTERNAL_REQUESTS, false)
 }
