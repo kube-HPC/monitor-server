7751b69f5d02315da47a2c06518df1d709809582
yehiyam <yehiyam@users.noreply.github.com>
2021-08-29 10:50:51 +0300

    fluentbit (#49)
    * update logger for fluent bit
    
    * update packages

--- config/logger/config.development.js
+++ config/logger/config.development.js
@@ -1,9 +1,8 @@
 const config = {};
 config.transport = {
     console: true,
-    fluentd: false,
-    logstash: false,
-    file: false
 };
-config.verbosityLevel = process.env.HKUBE_LOG_LEVEL || 2;
+config.console = {
+    colors: true,
+};
 module.exports = config;
